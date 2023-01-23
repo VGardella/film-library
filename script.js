ba5bf39b414965ac9414375776f6db173d8d8542
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-01-23 11:14:00 -0300

    Create constructor

---
+++ script.js
@@ -0,0 +1,9 @@
+let filmLibrary = [];
+
+function Movie(title, genre, description, director, year) {
+    this.title = title;
+    this.genre = genre;
+    this.description = description;
+    this.director = director;
+    this.year = year;
+}
