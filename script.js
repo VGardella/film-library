b178df08713b81094782a5257d97daff2f694e52
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-01-24 14:07:32 -0300

    Delete 'director' property from constructor

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let filmLibrary = [];
 const board = document.getElementById('board');
 
-function Movie(title, genre, description, director, year, seen) {
+function Movie(title, genre, description, year, seen) {
   this.title = title;
   this.genre = genre;
   this.description = description;
-  this.director = director;
   this.year = year;
   this.seen = seen;
 }
@@ -20,7 +19,6 @@ const movie1 = new Movie(
     "The Shining",
     "Psycological Horror",
     "Based on based on Stephen King's 1977 novel of the same name; an aspiring writer and recovering alcoholic accepts a position as the off-season caretaker of the isolated historic Overlook Hotel",
-    "Stanley Kubrick",
     "1980",
     true
 );
@@ -31,7 +29,6 @@ const movie2 = new Movie(
     "Big Trouble in Little China",
     "Fantasy, Action, Comedy",
     "Truck driver Jack Burton helps his friend Wang Chi rescue Wang's green-eyed fiancée from bandits in San Francisco's Chinatown",
-    "John Carpenter",
     "1986",
     true
 );
@@ -42,7 +39,6 @@ const movie3 = new Movie(
     "The Evil Dead",
     "Supernatural Horror",
     "Five college students vacationing in an isolated cabin find an audio tape that, when played, releases a legion of demons and spirits",
-    "Sam Raimi",
     "1981",
     true
 );
@@ -53,7 +49,6 @@ const movie4 = new Movie(
     "Demolition Man",
     "Science Fiction, Action",
     "A risk-taking police officer cryogenically frozen is awaken to help capture a extremely dangerous crime lord",
-    "Marco Brambilla",
     "1993",
     true
 );
@@ -64,7 +59,6 @@ const movie5 = new Movie(
     "Dredd",
     "Science Fiction, Action",
     "Judge Dredd, a law enforcer, and his apprentice partner, Judge Anderson, are forced to bring order to a 200-storey high-rise block of apartments and deal with its resident drug lord",
-    "Pete Travis",
     "2012",
     true
 );
@@ -75,7 +69,6 @@ const movie6 = new Movie(
     "The Running Man",
     "Action",
     "An man wrongly accused of a crime must participate in a television show where convicted criminal must escape death at the hands of professional killers",
-    "Paul Michael Glaser",
     "1987",
     true
 );
@@ -88,7 +81,6 @@ const movie7 = new Movie(
     "Blade Runner",
     "Science Fiction",
     "When a fugitive group of syntetic humans, or 'replicants', escapes to Earth, burnt-out cop Rick Deckard reluctantly agrees to hunt them down",
-    "Ridley Scott",
     "1982",
     false
 );
@@ -99,7 +91,6 @@ const movie8 = new Movie(
     "Tron",
     "Science Fiction, Action, Adventure",
     "A computer programmer and video game developer is transported inside the software world, where he interacts with programs in his attempt to escape",
-    "Ridley Scott",
     "1982",
     false
 );
@@ -110,7 +101,6 @@ const movie9 = new Movie(
     "Jaws",
     "Thriller",
     "A police chief, a marine biologist and a professional shark hunter persue a man-eating great white shark that attack beachgoers at a summer resort town",
-    "Steven Spielberg",
     "1975",
     false
 );
@@ -171,3 +161,4 @@ for (let i = 0; i < Object.keys(filmLibrary).length; i++) {
     movieButtons.appendChild(editBtn);
     movieButtons.appendChild(deleteBtn);
 };
+
