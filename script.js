35bed5c26c9bddda18135a1a07c379054e831b06
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-02-06 16:33:48 -0300

    Change array content to constructor objects

--- script.js
+++ script.js
@@ -12,24 +12,24 @@ function Movie(title, genre, description, year, seen, id) {
 // Entries
 
 let filmLibrary = [
-    {title: "The Shining", genre: "Horror", description: "Based on based on Stephen King's 1977 novel of the same name; an aspiring writer and recovering alcoholic accepts a position as the off-season caretaker of the isolated historic Overlook Hotel",
-year: "1980", seen: "true", id: 1},
-    {title: "Big Trouble in Little China", genre: "Fantasy, Action, Comedy", description: "Truck driver Jack Burton helps his friend Wang Chi rescue Wang's green-eyed fiancée from bandits in San Francisco's Chinatown",
-year: "1986", seen: "true", id: 2},
-    {title: "The Evil Dead", genre: "Horror", description: "Five college students vacationing in an isolated cabin find an audio tape that, when played, releases a legion of demons and spirits",
-year: "1981", seen: "true", id: 3},
-    {title: "Demolition Man", genre: "Science Fiction, Action", description: "A risk-taking police officer cryogenically frozen is awaken to help capture a extremely dangerous crime lord",
-year: "1993", seen: "true", id: 4},
-    {title: "Dredd", genre: "Science Fiction, Action", description: "Judge Dredd, a law enforcer, and his apprentice partner, Judge Anderson, are forced to bring order to a 200-storey high-rise block of apartments and deal with its resident drug lord",
-year: "2012", seen: "true", id: 5},
-    {title: "The Running Man", genre: "Action", description: "An man wrongly accused of a crime must participate in a television show where convicted criminal must escape death at the hands of professional killers",
-year: "1987", seen: "true", id: 6},
-    {title: "Blade Runner", genre: "Science Fiction", description: "When a fugitive group of syntetic humans, or 'replicants', escapes to Earth, burnt-out cop Rick Deckard reluctantly agrees to hunt them down",
-year: "1982", seen: "false", id: 7},
-    {title: "Tron", genre: "Science Fiction, Action, Adventure", description: "A computer programmer and video game developer is transported inside the software world, where he interacts with programs in his attempt to escape",
-year: "1982", seen: "false", id: 8},
-    {title: "Jaws", genre: "Thriller", description: "A police chief, a marine biologist and a professional shark hunter persue a man-eating great white shark that attack beachgoers at a summer resort town",
-year: "1975", seen: "false", id: 9}]
+    new Movie("The Shining", "Horror", "Based on based on Stephen King's 1977 novel of the same name; an aspiring writer and recovering alcoholic accepts a position as the off-season caretaker of the isolated historic Overlook Hotel",
+ "1980", "true", 1),
+    new Movie("Big Trouble in Little China", "Fantasy, Action, Comedy", "Truck driver Jack Burton helps his friend Wang Chi rescue Wang's green-eyed fiancée from bandits in San Francisco's Chinatown",
+"1986", "true", 2),
+    new Movie("The Evil Dead", "Horror", "Five college students vacationing in an isolated cabin find an audio tape that, when played, releases a legion of demons and spirits",
+"1981", "true", 3),
+    new Movie("Demolition Man", "Science Fiction, Action", "A risk-taking police officer cryogenically frozen is awaken to help capture a extremely dangerous crime lord",
+"1993", "true", 4),
+    new Movie("Dredd", "Science Fiction, Action", "Judge Dredd, a law enforcer, and his apprentice partner, Judge Anderson, are forced to bring order to a 200-storey high-rise block of apartments and deal with its resident drug lord",
+"2012", "true", 5),
+    new Movie("The Running Man", "Action", "An man wrongly accused of a crime must participate in a television show where convicted criminal must escape death at the hands of professional killers",
+"1987", "true", 6),
+    new Movie("Blade Runner", "Science Fiction", "When a fugitive group of syntetic humans, or 'replicants', escapes to Earth, burnt-out cop Rick Deckard reluctantly agrees to hunt them down",
+"1982", "false", 7),
+    new Movie("Tron", "Science Fiction, Action, Adventure", "A computer programmer and video game developer is transported inside the software world, where he interacts with programs in his attempt to escape",
+"1982", "false", 8),
+    new Movie("Jaws", "Thriller", "A police chief, a marine biologist and a professional shark hunter persue a man-eating great white shark that attack beachgoers at a summer resort town",
+"1975", "false", 9)]
 
 const seenBoard = document.getElementById('seen-board');
 const unseenBoard = document.getElementById('unseen-board');
@@ -195,79 +195,7 @@ function addMovieToList(event) {
     addMovie(newMovie);
     console.log(filmLibrary);
 
-    // Create card
-    let card = document.createElement('div');
-    card.setAttribute('class', 'card');
-    card.setAttribute('id', `${newMovie['id']}`)
-
-    // Create elements
-    let movieTitle = document.createElement('div');
-    movieTitle.setAttribute('class', 'movie-title');
-    let movieData = document.createElement('div');
-    movieData.setAttribute('class', 'movie-data');
-    let movieGenre = document.createElement('div');
-    movieGenre.setAttribute('class', 'movie-genre');
-    let movieYear = document.createElement('div');
-    movieYear.setAttribute('class', 'movie-year');
-    let movieDesc = document.createElement('div');
-    movieDesc.setAttribute('class', 'movie-desc');
-    let movieButtons = document.createElement('div');
-    movieButtons.setAttribute('class', 'movie-buttons');
-
-    let seenBtn = document.createElement('img');
-
-    let editBtn = document.createElement('img');
-    editBtn.classList.add('btn', 'edit');
-    editBtn.src = 'static/edit.png';
-    let deleteBtn = document.createElement('img');
-    deleteBtn.classList.add('btn', 'delete');
-    deleteBtn.src = 'static/delete.png';
-
-    // Add content
-    movieTitle.textContent = `${newMovie['title']}`;
-    movieGenre.textContent = `${newMovie['genre']}`;
-    movieYear.textContent = `${newMovie['year']}`;
-    movieDesc.textContent = `${newMovie['description']}`;
-
-    // Append containers
-    card.appendChild(movieTitle);
-    card.appendChild(movieData);
-    card.appendChild(movieDesc);
-    card.appendChild(movieButtons);
-
-    movieData.appendChild(movieGenre);
-    movieData.appendChild(movieYear);
-
-    movieButtons.appendChild(editBtn);
-    movieButtons.appendChild(deleteBtn);
-
-    // Append to board
-    if (newMovie['seen'] === 'true') {
-        card.classList.add('seen');
-
-        seenBtn.src = 'static/unseen.png';
-        seenBtn.classList.add('btn', 'seen-btn', 'seen');
-        movieButtons.appendChild(seenBtn);
-        
-        seenBoard.appendChild(card);
-    }
-    else if (newMovie['seen'] === 'false') {
-        card.classList.add('unseen');
-        
-        seenBtn.src = 'static/seen.png';
-        seenBtn.classList.add('btn', 'seen-btn', 'unseen');
-        movieButtons.appendChild(seenBtn);
-        
-        unseenBoard.appendChild(card);
-    }
-    modal.style.display = 'none';
-
-    // Add event Listeners
-    seenBtn.addEventListener('click', seenMovie);
-    deleteBtn.addEventListener('click', deleteMovie);
-    // editBtn.addEventListener('click', editMovie());
-
-    alert('Movie entry added correctly!')
+    addMovieCard();
 
     modal.style.display = 'none';
 
