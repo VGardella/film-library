af6bc8d1c1158a4cb8da98a1e7550c511ae4effa
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-02-06 15:34:40 -0300

    Change entries

--- script.js
+++ script.js
@@ -11,91 +11,26 @@ function Movie(title, genre, description, year, seen, id) {
 
 // Entries
 
-let movie1 = new Movie(
-    "The Shining",
-    "Psycological Horror",
-    "Based on based on Stephen King's 1977 novel of the same name; an aspiring writer and recovering alcoholic accepts a position as the off-season caretaker of the isolated historic Overlook Hotel",
-    "1980",
-    "true",
-    1
-);
+let filmLibrary = [
+    {title: "The Shining", genre: "Horror", description: "Based on based on Stephen King's 1977 novel of the same name; an aspiring writer and recovering alcoholic accepts a position as the off-season caretaker of the isolated historic Overlook Hotel",
+year: "1980", seen: "true", id: 1},
+    {title: "Big Trouble in Little China", genre: "Fantasy, Action, Comedy", description: "Truck driver Jack Burton helps his friend Wang Chi rescue Wang's green-eyed fiancée from bandits in San Francisco's Chinatown",
+year: "1986", seen: "true", id: 2},
+    {title: "The Evil Dead", genre: "Horror", description: "Five college students vacationing in an isolated cabin find an audio tape that, when played, releases a legion of demons and spirits",
+year: "1981", seen: "true", id: 3},
+    {title: "Demolition Man", genre: "Science Fiction, Action", description: "A risk-taking police officer cryogenically frozen is awaken to help capture a extremely dangerous crime lord",
+year: "1993", seen: "true", id: 4},
+    {title: "Dredd", genre: "Science Fiction, Action", description: "Judge Dredd, a law enforcer, and his apprentice partner, Judge Anderson, are forced to bring order to a 200-storey high-rise block of apartments and deal with its resident drug lord",
+year: "2012", seen: "true", id: 5},
+    {title: "The Running Man", genre: "Action", description: "An man wrongly accused of a crime must participate in a television show where convicted criminal must escape death at the hands of professional killers",
+year: "1987", seen: "true", id: 6},
+    {title: "Blade Runner", genre: "Science Fiction", description: "When a fugitive group of syntetic humans, or 'replicants', escapes to Earth, burnt-out cop Rick Deckard reluctantly agrees to hunt them down",
+year: "1982", seen: "false", id: 7},
+    {title: "Tron", genre: "Science Fiction, Action, Adventure", description: "A computer programmer and video game developer is transported inside the software world, where he interacts with programs in his attempt to escape",
+year: "1982", seen: "false", id: 8},
+    {title: "Jaws", genre: "Thriller", description: "A police chief, a marine biologist and a professional shark hunter persue a man-eating great white shark that attack beachgoers at a summer resort town",
+year: "1975", seen: "false", id: 9}]
 
-let movie2 = new Movie(
-    "Big Trouble in Little China",
-    "Fantasy, Action, Comedy",
-    "Truck driver Jack Burton helps his friend Wang Chi rescue Wang's green-eyed fiancée from bandits in San Francisco's Chinatown",
-    "1986",
-    "true",
-    2
-);
-
-let movie3 = new Movie(
-    "The Evil Dead",
-    "Supernatural Horror",
-    "Five college students vacationing in an isolated cabin find an audio tape that, when played, releases a legion of demons and spirits",
-    "1981",
-    "true",
-    3
-);
-
-let movie4 = new Movie(
-    "Demolition Man",
-    "Science Fiction, Action",
-    "A risk-taking police officer cryogenically frozen is awaken to help capture a extremely dangerous crime lord",
-    "1993",
-    "true",
-    4
-);
-
-let movie5 = new Movie(
-    "Dredd",
-    "Science Fiction, Action",
-    "Judge Dredd, a law enforcer, and his apprentice partner, Judge Anderson, are forced to bring order to a 200-storey high-rise block of apartments and deal with its resident drug lord",
-    "2012",
-    "true",
-    5
-);
-
-let movie6 = new Movie(
-    "The Running Man",
-    "Action",
-    "An man wrongly accused of a crime must participate in a television show where convicted criminal must escape death at the hands of professional killers",
-    "1987",
-    "true",
-    6
-);
-  
-// To be seen
-
-let movie7 = new Movie(
-    "Blade Runner",
-    "Science Fiction",
-    "When a fugitive group of syntetic humans, or 'replicants', escapes to Earth, burnt-out cop Rick Deckard reluctantly agrees to hunt them down",
-    "1982",
-    "false",
-    7
-);
-
-let movie8 = new Movie(
-    "Tron",
-    "Science Fiction, Action, Adventure",
-    "A computer programmer and video game developer is transported inside the software world, where he interacts with programs in his attempt to escape",
-    "1982",
-    "false",
-    8
-);
-
-let movie9 = new Movie(
-    "Jaws",
-    "Thriller",
-    "A police chief, a marine biologist and a professional shark hunter persue a man-eating great white shark that attack beachgoers at a summer resort town",
-    "1975",
-    "false",
-    9
-);
-
-
-let filmLibrary = [movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9];
 const seenBoard = document.getElementById('seen-board');
 const unseenBoard = document.getElementById('unseen-board');
 
@@ -105,74 +40,77 @@ function addMovie(object) {
 
 // Create cards
 
-for (let i = 0; i < Object.keys(filmLibrary).length; i++) {
+let addMovie = function() {
+    for (let i = 0; i < Object.keys(filmLibrary).length; i++) {
     
-    // Create card
-    let card = document.createElement('div');
-    card.setAttribute('class', 'card');
-    card.setAttribute('id', `${filmLibrary[i]['id']}`)
+        // Create card
+        let card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        card.setAttribute('id', `${filmLibrary[i]['id']}`)
+    
+        // Create elements
+        let movieTitle = document.createElement('div');
+        movieTitle.setAttribute('class', 'movie-title');
+        let movieData = document.createElement('div');
+        movieData.setAttribute('class', 'movie-data');
+        let movieGenre = document.createElement('div');
+        movieGenre.setAttribute('class', 'movie-genre');
+        let movieYear = document.createElement('div');
+        movieYear.setAttribute('class', 'movie-year');
+        let movieDesc = document.createElement('div');
+        movieDesc.setAttribute('class', 'movie-desc');
+        let movieButtons = document.createElement('div');
+        movieButtons.setAttribute('class', 'movie-buttons');
+    
+        let seenBtn = document.createElement('img');
+    
+        let editBtn = document.createElement('img');
+        editBtn.classList.add('btn', 'edit');
+        editBtn.src = 'static/edit.png';
+        let deleteBtn = document.createElement('img');
+        deleteBtn.classList.add('btn', 'delete');
+        deleteBtn.src = 'static/delete.png';
+    
+        // Add content
+        movieTitle.textContent = `${filmLibrary[i]['title']}`;
+        movieGenre.textContent = `${filmLibrary[i]['genre']}`;
+        movieYear.textContent = `${filmLibrary[i]['year']}`;
+        movieDesc.textContent = `${filmLibrary[i]['description']}`;
+    
+        // Append containers
+        card.appendChild(movieTitle);
+        card.appendChild(movieData);
+        card.appendChild(movieDesc);
+        card.appendChild(movieButtons);
+    
+        movieData.appendChild(movieGenre);
+        movieData.appendChild(movieYear);
+    
+        movieButtons.appendChild(editBtn);
+        movieButtons.appendChild(deleteBtn);
+    
+        // Append to board
+        if (filmLibrary[i]['seen'] === 'true') {
+            card.classList.add('seen');
+    
+            seenBtn.src = 'static/unseen.png';
+            seenBtn.classList.add('btn', 'seen-btn', 'seen');
+            movieButtons.appendChild(seenBtn);
+            
+            seenBoard.appendChild(card);
+        }
+        else if (filmLibrary[i]['seen'] === 'false') {
+            card.classList.add('unseen');
+            
+            seenBtn.src = 'static/seen.png';
+            seenBtn.classList.add('btn', 'seen-btn', 'unseen');
+            movieButtons.appendChild(seenBtn);
+            
+            unseenBoard.appendChild(card);
+        }
+    };
+}
 
-    // Create elements
-    let movieTitle = document.createElement('div');
-    movieTitle.setAttribute('class', 'movie-title');
-    let movieData = document.createElement('div');
-    movieData.setAttribute('class', 'movie-data');
-    let movieGenre = document.createElement('div');
-    movieGenre.setAttribute('class', 'movie-genre');
-    let movieYear = document.createElement('div');
-    movieYear.setAttribute('class', 'movie-year');
-    let movieDesc = document.createElement('div');
-    movieDesc.setAttribute('class', 'movie-desc');
-    let movieButtons = document.createElement('div');
-    movieButtons.setAttribute('class', 'movie-buttons');
-
-    let seenBtn = document.createElement('img');
-
-    let editBtn = document.createElement('img');
-    editBtn.classList.add('btn', 'edit');
-    editBtn.src = 'static/edit.png';
-    let deleteBtn = document.createElement('img');
-    deleteBtn.classList.add('btn', 'delete');
-    deleteBtn.src = 'static/delete.png';
-
-    // Add content
-    movieTitle.textContent = `${filmLibrary[i]['title']}`;
-    movieGenre.textContent = `${filmLibrary[i]['genre']}`;
-    movieYear.textContent = `${filmLibrary[i]['year']}`;
-    movieDesc.textContent = `${filmLibrary[i]['description']}`;
-
-    // Append containers
-    card.appendChild(movieTitle);
-    card.appendChild(movieData);
-    card.appendChild(movieDesc);
-    card.appendChild(movieButtons);
-
-    movieData.appendChild(movieGenre);
-    movieData.appendChild(movieYear);
-
-    movieButtons.appendChild(editBtn);
-    movieButtons.appendChild(deleteBtn);
-
-    // Append to board
-    if (filmLibrary[i]['seen'] === 'true') {
-        card.classList.add('seen');
-
-        seenBtn.src = 'static/unseen.png';
-        seenBtn.classList.add('btn', 'seen-btn', 'seen');
-        movieButtons.appendChild(seenBtn);
-        
-        seenBoard.appendChild(card);
-    }
-    else if (filmLibrary[i]['seen'] === 'false') {
-        card.classList.add('unseen');
-        
-        seenBtn.src = 'static/seen.png';
-        seenBtn.classList.add('btn', 'seen-btn', 'unseen');
-        movieButtons.appendChild(seenBtn);
-        
-        unseenBoard.appendChild(card);
-    }
-};
 
 function addMovieToList(event) {
     event.preventDefault();
